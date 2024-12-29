1f0b0191cc5867cd6eb9d7d1644e8a539566e5d1
Raymond <20248577+javaarchive@users.noreply.github.com>
2024-12-28 18:38:42 -0800

    3.0 feature update with searching match + finetuned matching

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # lrcsync
 lrclib.net client, basically looks in current directory for audio files and pulls the lrc for the song if it exists from lrclib.net. Quick poc atm but I have this because I can't run lrcget on a remote server lol because it has no gui.
+
+## Usage
+Simply run:
+```
+lrcsync
+```
+in a directory with audio files and for audio files with metadata it'll look them up on lrclib.net and if there is a specific match it'll pull them.
+
+For a more "loose" search you can use the `--search` flag, this will use the search endpoint which can account for different punctuation, misspellings, and case. You can also use `--ignore` to ignore certain properties when searching, for example to ignore the artist name you can do:
+```bash
+lrcsync --search --ignore artist --tolerance 3.0
+```
+When search is used as a fallback it will try to match by closest duration. The `--tolerance` flag can be used to set a tolerance in seconds, any results exceeding this threshold will be ignored.
+
+```
+Usage: lrcsync [OPTIONS]
+
+Options:
+  -u, --lrclib-url <LRCLIB_URL>  [default: https://lrclib.net]
+  -a, --hidden
+  -f, --force                    overwrite existing lrc files
+  -i, --ignore <IGNORE>          ignore the follow properties when searching lrclib by not sending them, comma seperated
+  -s, --search                   use searching on lrclib as a fallback
+  -t, --tolerance <TOLERANCE>    tolerance in seconds for searching lrclib [default: 5]
+  -h, --help                     Print help
+  -V, --version                  Print version
+```
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 
+use anyhow::bail;
 use audiotags::Tag;
 use clap::Parser;
-use ignore::WalkBuilder;
+use ignore::{DirEntry, WalkBuilder};
 use serde::{Serialize, Deserialize};
 use tokio::{fs::File, io::AsyncWriteExt};
 
@@ -15,7 +16,11 @@ pub struct CliConfig {
     #[arg(short = 'f', long = "force", default_value_t = false, help = "overwrite existing lrc files")]
     pub force: bool,
     #[arg(short = 'i', long = "ignore", value_parser, num_args = 1, help = "ignore the follow properties when searching lrclib by not sending them, comma seperated")]
-    pub ignore: Vec<String>
+    pub ignore: Vec<String>,
+    #[arg(short = 's', long = "search", default_value_t = false, help = "use searching on lrclib as a fallback")]
+    pub search: bool,
+    #[arg(short = 't', long = "tolerance", default_value_t = 5.0, help = "tolerance in seconds for searching lrclib")]
+    pub tolerance: f32,
 }
 
 static DEFAULT_USER_AGENT: &str = concat!(
@@ -42,7 +47,7 @@ pub struct LrclibQuery {
 
 impl LrclibQuery {
     // old method
-    pub fn to_query_string(&self) -> String {
+    pub fn to_get_query_string(&self) -> String {
         let mut query = String::new();
         query.push_str("track_name=");
         query.push_str(&self.track_name);
@@ -59,15 +64,22 @@ impl LrclibQuery {
         query
     }
 
-    pub fn to_query(&self) -> Vec<(String, String)> {
-        let mut query = Vec::new();
-        query.push(("track_name".to_string(), self.track_name.clone()));
-        query.push(("artist_name".to_string(), self.artist_name.clone()));
-        if let Some(album_name) = &self.album_name {
-            query.push(("album_name".to_string(), album_name.clone()));
-        }
+    pub fn to_get_query(&self) -> Vec<(String, String)> {
+        let mut query = self.to_search_query();
         if let Some(duration) = &self.duration {
             query.push(("duration".to_string(), duration.to_string()));
+        }
+        query
+    }
+
+    pub fn to_search_query(&self) -> Vec<(String, String)> {
+        let mut query = Vec::new();
+        query.push(("track_name".to_string(), self.track_name.clone()));
+        if self.artist_name.len() > 0 {
+            query.push(("artist_name".to_string(), self.artist_name.clone()));
+        }
+        if let Some(album_name) = &self.album_name {
+            query.push(("album_name".to_string(), album_name.clone()));
         }
         query
     }
@@ -109,7 +121,7 @@ impl LrcLibClient {
 
     pub async fn get(&self, query: &LrclibQuery) -> anyhow::Result<Option<LrclibItem>> {
         let url = format!("{}/api/get" ,self.url);
-        let request_builder = self.client.get(url).query(&query.to_query());
+        let request_builder = self.client.get(url).query(&query.to_get_query());
         let response = request_builder.send().await?;
         if response.status().is_success() {
             let body = response.text().await?;
@@ -127,8 +139,50 @@ impl LrcLibClient {
             }
         }
     }
+
+    pub async fn search(&self, query: &LrclibQuery) -> anyhow::Result<Option<Vec<LrclibItem>>> {
+        let url = format!("{}/api/search" ,self.url);
+        let request_builder = self.client.get(url).query(&query.to_search_query());
+        let response = request_builder.send().await?;
+        if response.status().is_success() {
+            let body = response.text().await?;
+            match serde_json::from_str::<Vec<LrclibItem>>(&body) {
+                Ok(items) => Ok(Some(items)), 
+                Err(err) => {
+                    anyhow::bail!("Error parsing lrclib response (did the api schema change?): {}", err);
+                },
+            }
+        } else {
+            if response.status() == reqwest::StatusCode::NOT_FOUND {
+                Ok(None)
+            } else {
+                Err(anyhow::anyhow!("Error getting lrclib item: {}", response.status()))
+            }
+        }
+    }
 }
 
+pub async fn write_lrc_for_file(entry: &DirEntry, synced_lyrics: &str, config: &CliConfig) -> anyhow::Result<()> {
+    let lrc_path = entry.path().with_extension("lrc");
+
+    match File::create(lrc_path).await {
+        Ok(mut file) => {
+            match file.write_all(synced_lyrics.as_bytes()).await {
+                Ok(_) => {
+                    println!("Wrote synced lrc to {}", entry.path().display());
+                },
+                Err(err) => {
+                    bail!("Writing file failed: {}", err);
+                }
+            }
+        },
+        Err(err) => {
+            bail!("Creating file failed: {}", err);
+        }
+    }
+
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() {
@@ -181,7 +235,7 @@ async fn main() {
                         if config.ignore.contains(&"duration".to_string()) {
                             lrc_query.remove_duration();
                         }
-                        if config.ignore.contains(&"album_name".to_string()) {
+                        if config.ignore.contains(&"album_name".to_string()) || config.ignore.contains(&"album".to_string()) {
                             lrc_query.remove_album_name();
                         }
                         match client.get(&lrc_query).await {
@@ -189,25 +243,69 @@ async fn main() {
                                 if let Some(synced_lyrics) = &lrc_item.syncedLyrics {
                                     println!("Found synced lrc for {}", entry.path().display());
                                     // write to file with extension changed to .lrc
-                                    match File::create(entry.path().with_extension("lrc")).await {
-                                        Ok(mut file) => {
-                                            match file.write_all(synced_lyrics.as_bytes()).await {
-                                                Ok(_) => {
-                                                    println!("Wrote synced lrc to {}", entry.path().display());
-                                                },
-                                                Err(err) => {
-                                                    println!("Error writing file {}: {}",entry.path().display(), err);
-                                                }
-                                            }
-                                        },
+                                    match write_lrc_for_file(&entry, synced_lyrics, &config).await {
+                                        Ok(_) => {},
                                         Err(err) => {
-                                            println!("Error creating file {}: {}",entry.path().display(), err);
+                                            println!("Error in saving lrc {}: {}",entry.path().display(), err);
                                         }
                                     }
                                 }
                             },
                             Ok(None) => {
-                                println!("Did not find lrc for {}",entry.path().display()); 
+                                if config.search {
+                                    // search fallback
+                                    println!("Searching lrc for {}", entry.path().display());
+                                    // hide artist hack
+                                    if config.ignore.contains(&"artist_name".to_string()) || config.ignore.contains(&"artist".to_string()) {
+                                        lrc_query.artist_name = "".to_string();
+                                    }
+                                    match client.search(&lrc_query).await {
+                                        Ok(Some(lrc_items)) => {
+                                            // weird order but it works and avoids too much nesting
+                                            if lrc_items.len() == 0 {
+                                                println!("Did not find lrc for (no results) {}",entry.path().display());
+                                            } else {
+                                                let mut canidates = lrc_items;
+                                                if let Some(duration) = &lrc_query.duration {
+                                                    // sort by closed to target duration
+                                                    canidates.sort_by(|a, b| {
+                                                        let a_duration = a.duration as f32;
+                                                        let b_duration = b.duration as f32;
+                                                        let a_delta = a_duration - duration;
+                                                        let b_delta = b_duration - duration;
+                                                        return a_delta.abs().partial_cmp(&b_delta.abs()).unwrap();
+                                                    }); 
+                                                
+                                                    if config.tolerance > 0.0 {
+                                                        canidates = canidates.into_iter().filter(|item| {
+                                                            let item_duration = item.duration as f32;
+                                                            let delta = item_duration - duration;
+                                                            return delta.abs() < config.tolerance;
+                                                        }).collect();
+                                                    }
+                                                }
+                                                
+                                                println!("Searched lrc (found {}secs vs actual {}secs out of {} filtered results) for {}",canidates[0].duration,lrc_query.duration.unwrap_or(-1.0), canidates.len(), entry.path().display());  
+                                                // write to file with extension changed to .lrc
+                                                // TODO: manual duration tolerance?
+                                                match write_lrc_for_file(&entry, &canidates[0].syncedLyrics.as_ref().unwrap(), &config).await {
+                                                    Ok(_) => {},
+                                                    Err(err) => {
+                                                        println!("Error in saving lrc {}: {}",entry.path().display(), err);
+                                                    }
+                                                }
+                                            }
+                                        },
+                                        Ok(None) => {
+                                            println!("Did not find lrc for {}",entry.path().display()); 
+                                        },
+                                        Err(err) => {
+                                            println!("Error searching lrc for {}: {}",entry.path().display(), err);
+                                        }
+                                    }
+                                } else {
+                                    println!("Did not find lrc for {}",entry.path().display()); 
+                                }
                             }
                             Err(err) => {
                                 println!("Error finding lrc for {}: {}",entry.path().display(), err);
